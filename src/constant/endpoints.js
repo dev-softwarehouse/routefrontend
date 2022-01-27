286cfe1293bd1137eaf08e6d12840c4df473d56d
dev-softwarehouse <pawel.softwarehouse@gmail.com>
2022-01-27 14:52:02 +0100

    changed endpoints

--- src/constant/endpoints.js
+++ src/constant/endpoints.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT_HTTP = (process.env.NODE_ENV === 'production') ? 'https://rc-server-2708.herokuapp.com' : 'http://localhost:8010'
-export const API_ENDPOINT_WS = (process.env.NODE_ENV === 'production') ? 'wss://rc-server-2708.herokuapp.com' : 'ws://localhost:8010'
+export const API_ENDPOINT_HTTP = 'https://route-back-end.herokuapp.com'
+export const API_ENDPOINT_WS = 'https://route-back-end.herokuapp.com'
